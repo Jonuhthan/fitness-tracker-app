6de561aa56b8e7319e224aeb4ce7f3b9db9a980a
Jonathan Lam <jonhaolam@gmail.com>
2024-03-06 13:11:15 -0500

    Add barcode scanning feature
    When user opens the site, the video stream also checks for barcodes and will end when it scans one

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, Response
 from imutils.video import VideoStream
 import cv2
 import time
+import pyzbar
 
 app = Flask(__name__)
 
@@ -21,6 +22,15 @@ def generate():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         time.sleep(0.1)
+
+        found = []
+        barcodes = pyzbar.decode(frame)
+        for barcode in barcodes:
+            barcodeData = barcode.data.decode("utf-8")
+            found.append(barcodeData)
+            
+        if len(found) > 0:
+            break
 
 # Home page
 @app.route('/')
