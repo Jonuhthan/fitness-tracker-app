55c88b642dfcfe9ed7d7fbbe243b780d67dbfc97
Jonathan Lam <jonhaolam@gmail.com>
2024-03-08 16:14:23 -0500

    Removed stop button and function
    Fixed bug with repeated Start presses and ends stream when barcode is scanned. Stop button removed to streamline app usage.

--- app.py
+++ app.py
@@ -3,33 +3,35 @@ from imutils.video import VideoStream
 import cv2
 from pyzbar import pyzbar
 import threading
+import time
 
 app = Flask(__name__)
 vs = None
-output_frame = None
 found = []
 lock = threading.Lock()
 
 def generate():
-    global vs, output_frame
+    global vs
 
     while True:
         with lock:
             if vs is None:
+                print('Video Stream returned None.')
                 break
-            
+                    
             # Read each frame and make sure it works
-            (success, frame) = vs.read()
+            success, frame = vs.read()
+            
+            if not success:
+                print('Failed to read frame.')
+                break
 
-            # Check for barcodes
             barcodes = pyzbar.decode(frame)
             for barcode in barcodes:
                 barcodeData = barcode.data.decode("utf-8")
                 # Append first found barcode to global found
                 found.append(barcodeData)
-            if not success:
-                break
-
+            
             _, buffer = cv2.imencode('.jpg', frame)
             # Break the frame down into bytes
             frame = buffer.tobytes()
@@ -40,38 +42,33 @@ def generate():
 
             # Stops generating frames when barcode is found
             if len(found) > 0:
+                print(f"Barcode scanned: {barcodeData}")
                 break
-
+    # Close video stream
     vs.release()
     cv2.destroyAllWindows()
+
 
 # Home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Calls generate() to display jpg frames on HTML page
 @app.route('/video_feed')
 def video_feed():
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def start_feed():
     global vs
-    vs = cv2.VideoCapture(cv2.CAP_DSHOW)
-
-def stop_feed():
-    global vs
-    if vs is not None:
-        vs.release()
+    if vs is None:  # Prevents Video from breaking after repeated presses
+        vs = cv2.VideoCapture(cv2.CAP_DSHOW)
 
 @app.route('/start_feed', methods=['POST'])
 def start_feed_route():
     start_feed()
     return "Starting video feed"
 
-@app.route('/stop_feed', methods=['POST'])
-def stop_feed_route():
-    stop_feed()
-    return "Stopping video feed"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -2,8 +2,3 @@ async function start_video(url) {
     await fetch("/start_feed", { method: "POST"});
     document.getElementById("placeholder").src = url;
 };
-
-async function stop_video(image) {
-    await fetch("/stop_feed", { method: "POST"});
-    document.getElementById("placeholder").src = image;
-};
